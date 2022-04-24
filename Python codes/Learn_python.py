38228b34d3dee464236f909cd4e2bce3a0491779
sindhucharan95 <102479676+sindhucharan95@users.noreply.github.com>
2022-04-24 17:18:59 +0200

    msg

---
+++ Python codes/Learn_python.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_excel (r'\Users\sindhucharan\Downloads\Start-Up-Loans-Cash-Flow-Forecast.xlsx')
+print (df)
+
+print("Hello.Git")
+print("Hello-world")
+print("Hello-world-again")
